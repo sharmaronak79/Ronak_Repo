2a38a1bdee4e3913a4cc56cbbccf4d6b154e71cd
Ronakkumar Sharma <Ronakkumar.Sharma@ccg.sanm.corp>
2024-05-17 15:52:34 -0400

    trying to compile and run C file also

--- opc_prac.c
+++ opc_prac.c
@@ -2,9 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+char *command;
+float freq;
+int power;
+}cmd_t;
+
+
 int main(){
 
-    printf("Lets play with C now");
+    printf("Lets play with structure now");
+    cmd_t var1;
+    var1.command="gen_freq";
+    var1.freq = 10000;
+    var1.power = 5;
+
+    printf("parsed command is %s",var1.command);
+    printf("Generate Frequency : %f",var1.freq);
+    printf("Required Pwer is : %d",var1.power);    
+
 
     return 0;
 }
